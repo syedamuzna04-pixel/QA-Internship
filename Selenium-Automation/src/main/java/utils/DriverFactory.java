35f4196eace5d5dd0b36968e72c5c53659654147
syedamuzna04-pixel <syedamuzna04@gmail.com>
2026-09-04 21:26:47 +0500

    Update DriverFactory.java

--- Selenium-Automation/src/main/java/utils/DriverFactory.java
+++ Selenium-Automation/src/main/java/utils/DriverFactory.java
@@ -1,1 +1,25 @@
+package utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+
+    public static WebDriver driver;
+
+    public static void initializeDriver() {
+
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    public static void quitDriver() {
+
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
