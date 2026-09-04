f4ff94d3be61bedf783d05f93dfaba7016d07d9e
syedamuzna04-pixel <syedamuzna04@gmail.com>
2026-09-04 21:20:58 +0500

    Update LoginPage.java

--- Selenium-Automation/src/main/java/pages/LoginPage.java
+++ Selenium-Automation/src/main/java/pages/LoginPage.java
@@ -1,1 +1,48 @@
+package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import utils.ElementUtils;
+
+public class LoginPage {
+
+    private ElementUtils elementUtils;
+
+    // Locators
+    private By usernameField = By.id("user-name");
+    private By passwordField = By.id("password");
+    private By loginButton = By.id("login-button");
+    private By errorMessage = By.cssSelector("[data-test='error']");
+
+    // Constructor
+    public LoginPage(WebDriver driver) {
+        elementUtils = new ElementUtils(driver);
+    }
+
+    // Enter username
+    public void enterUsername(String username) {
+        elementUtils.type(usernameField, username);
+    }
+
+    // Enter password
+    public void enterPassword(String password) {
+        elementUtils.type(passwordField, password);
+    }
+
+    // Click login
+    public void clickLogin() {
+        elementUtils.click(loginButton);
+    }
+
+    // Complete login action
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
+
+    // Get error message
+    public String getErrorMessage() {
+        return elementUtils.getText(errorMessage);
+    }
+}
