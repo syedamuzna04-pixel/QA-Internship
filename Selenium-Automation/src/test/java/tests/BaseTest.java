666010c75a5dc25769a292a2ce672c1722738945
syedamuzna04-pixel <syedamuzna04@gmail.com>
2026-09-04 21:29:37 +0500

    Implement BaseTest class for Selenium tests

--- Selenium-Automation/src/test/java/tests/BaseTest.java
+++ Selenium-Automation/src/test/java/tests/BaseTest.java
@@ -1,1 +1,25 @@
+package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utils.DriverFactory;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+
+        DriverFactory.initializeDriver();
+
+        driver = DriverFactory.driver;
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        DriverFactory.quitDriver();
+    }
+}
