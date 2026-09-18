0441758fbb7c91298dc7f52a00d16a82f7d4f99a
syedamuzna04-pixel <syedamuzna04@gmail.com>
2026-09-18 15:28:33 +0500

    Add DarazHomePage class for automation

--- Playwright Automation/pages/DarazHomePage.js
+++ Playwright Automation/pages/DarazHomePage.js
@@ -1,1 +1,16 @@
+export class DarazHomePage {
+  constructor(page) {
+    this.page = page;
+    this.searchBox = page.locator('input[type="search"]');
+  }
 
+  async navigate() {
+    await this.page.goto('https://www.daraz.pk/');
+  }
+
+  async searchProduct(productName) {
+    await this.searchBox.fill(productName);
+    await this.searchBox.press('Enter');
+    await this.page.waitForLoadState('domcontentloaded');
+  }
+}
