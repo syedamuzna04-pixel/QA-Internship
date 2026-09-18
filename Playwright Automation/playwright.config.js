13ae8ecef02d60418083359b2a36a8972c7a0b36
syedamuzna04-pixel <syedamuzna04@gmail.com>
2026-09-18 15:31:32 +0500

    Add files via upload

---
+++ Playwright Automation/playwright.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+
+  use: {
+    browserName: 'chromium',
+    channel: 'chrome',
+    headless: false,
+  },
+});
